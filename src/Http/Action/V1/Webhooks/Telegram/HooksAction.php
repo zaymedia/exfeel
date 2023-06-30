d3d3a1dcc8dec333801a7e39022a15b7e34bb10c
Konstantin Zaytcev <zaydisk@yandex.ru>
2023-06-30 22:32:05 +0300

    test

--- src/Http/Action/V1/Webhooks/Telegram/HooksAction.php
+++ src/Http/Action/V1/Webhooks/Telegram/HooksAction.php
@@ -8,10 +8,12 @@ use BotMan\BotMan\BotMan;
 use BotMan\BotMan\BotManFactory;
 use BotMan\BotMan\Drivers\DriverManager;
 use BotMan\Drivers\Telegram\TelegramDriver;
+use Exception;
 use OpenApi\Attributes as OA;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use ZayMedia\Shared\Components\Sentry;
 use ZayMedia\Shared\Http\Response\JsonDataSuccessResponse;
 
 #[OA\Get(
@@ -27,11 +29,14 @@ use ZayMedia\Shared\Http\Response\JsonDataSuccessResponse;
 final class HooksAction implements RequestHandlerInterface
 {
     public function __construct(
+        private readonly Sentry $sentry
     ) {
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        // https://api.telegram.org/bot6194699354:AAGsIq2wAHBi44A2Vzj3zeEMpBvZ77oSMy4/setwebhook?url=https://exfeel.zay.media/v1/webhooks/telegram/hooks
+
         $bot_api_key  = '6194699354:AAGsIq2wAHBi44A2Vzj3zeEMpBvZ77oSMy4';
         // $bot_username = 'exfeelbot';
 
@@ -42,6 +47,14 @@ final class HooksAction implements RequestHandlerInterface
                 'token' => $bot_api_key,
             ],
         ]);
+
+        $this->sentry->capture(
+            new Exception(json_encode($request->getParsedBody()))
+        );
+
+        $this->sentry->capture(
+            new Exception($botman->getMessage()->getText())
+        );
 
         $botman->hears('hello', function (BotMan $bot) {
             $bot->reply('Hello yourself.');
