4295d3510b244e08d8f6c9c8c8680fc1b4d782d7
Konstantin Zaytcev <zaydisk@yandex.ru>
2023-06-30 23:30:36 +0300

    test

--- src/Modules/Bot/Command/InstagramPhoto.php
+++ src/Modules/Bot/Command/InstagramPhoto.php
@@ -5,11 +5,20 @@ declare(strict_types=1);
 namespace App\Modules\Bot\Command;
 
 use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Messages\Attachments\Image;
+use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
 
 class InstagramPhoto
 {
     public function handleFoo(BotMan $bot): void
     {
-        $bot->reply('Hello World');
+        $attachment = new Image('https://img.freepik.com/free-photo/a-cupcake-with-a-strawberry-on-top-and-a-strawberry-on-the-top_1340-35087.jpg', [
+            'custom_payload' => true,
+        ]);
+
+        $message = OutgoingMessage::create('This is my text')
+            ->withAttachment($attachment);
+
+        $bot->reply($message);
     }
 }
