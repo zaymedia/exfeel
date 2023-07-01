18524f561c40323445c505e59fdcdf94f196bb5f
Konstantin Zaytcev <zaydisk@yandex.ru>
2023-07-01 11:35:02 +0300

    separate callbacks

--- src/Modules/Bot/Command/Webhook/WebhookHandler.php
+++ src/Modules/Bot/Command/Webhook/WebhookHandler.php
@@ -21,9 +21,9 @@ final class WebhookHandler
         // https://api.telegram.org/bot6194699354:AAGsIq2wAHBi44A2Vzj3zeEMpBvZ77oSMy4/setwebhook?url=https://exfeel.zay.media/v1/webhooks/telegram
         // https://github.com/php-telegram-bot/example-bot/blob/master/hook.php
 
-        $this->bot->hears('/start', StartCallback::class);
-        $this->bot->hears('/help', HelpCallback::class);
-        $this->bot->hears('photo', PhotoCallback::class);
+        $this->bot->hears('/start', StartCallback::class . '@handle');
+        $this->bot->hears('/help', HelpCallback::class . '@handle');
+        $this->bot->hears('photo', PhotoCallback::class . '@handle');
 
         $this->bot->hears('hello', function (BotMan $bot) {
             $bot->typesAndWaits(2);
