b089b6fcf6c95d56e13ba3887c97cb35a7b9190f
Konstantin Zaytcev <zaydisk@yandex.ru>
2023-07-05 19:55:23 +0300

    test edit

--- src/Console/Bot/WebhookCommand.php
+++ src/Console/Bot/WebhookCommand.php
@@ -11,17 +11,20 @@ use App\Console\Bot\Callbacks\Language\LanguageCallback;
 use App\Console\Bot\Callbacks\StartCallback;
 use App\Console\Bot\Callbacks\Subscribers\SubscriptionsCallback;
 use BotMan\BotMan\BotMan;
+use phpDocumentor\Reflection\Exception;
+use ZayMedia\Shared\Components\Sentry;
 
 final class WebhookCommand
 {
     public function __construct(
         private readonly BotMan $bot,
+        private readonly Sentry $sentry,
     ) {
     }
 
     public function handle(): void
     {
-        $this->bot->reply('(:');
+        $this->sentry->capture(new Exception(json_encode($this->bot->getMessage()->getText())));
 
         $this->bot->hears(StartCallback::getPattern(), StartCallback::class);
         $this->bot->hears(SubscriptionsCallback::getPattern(), SubscriptionsCallback::class);
